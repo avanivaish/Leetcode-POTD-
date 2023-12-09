87790763a584f83b542fb04cd6deed2ac9ffc715
Avani Vaish <77274574+avanivaish@users.noreply.github.com>
2023-12-09 13:37:54 +0530

    Add files via upload

---
+++ Dec 2023/dec9.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Definition for a binary tree node.
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+    void preorder(TreeNode* node, vector<int>& ans){
+        if(node != nullptr){
+            preorder(node->left, ans);
+            ans.push_back(node->val);
+            preorder(node->right, ans);
+        }
+    }
+
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        preorder(root, ans);
+        return ans;
+    }
+};
+
+int main() {
+    // Example usage
+    TreeNode* root = new TreeNode(1);
+    root->right = new TreeNode(2);
+    root->right->left = new TreeNode(3);
+
+    Solution solution;
+    vector<int> result = solution.inorderTraversal(root);
+
+    cout << "Inorder Traversal: ";
+    for (int val : result) {
+        cout << val << " ";
+    }
+    cout << endl;
+
+    // Clean up memory
+    delete root->right->left;
+    delete root->right;
+    delete root;
+
+    return 0;
+}
