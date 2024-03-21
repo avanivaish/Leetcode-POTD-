16ec9bba8400cf3335eac609afce06f2be9bf9ce
Avani Vaish <77274574+avanivaish@users.noreply.github.com>
2024-03-21 23:11:09 +0530

    Add files via upload

---
+++ March 2024/march21.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+using namespace std;
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* prev = nullptr;
+        ListNode* curr = head;
+        ListNode* nextptr;
+        while (curr != nullptr) {
+            nextptr = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextptr;
+        }
+        return prev;
+    }
+};
+
+// Helper function to print the linked list
+void printList(ListNode* head) {
+    while (head != nullptr) {
+        cout << head->val << " ";
+        head = head->next;
+    }
+    cout << endl;
+}
+
+int main() {
+    // Example usage
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    cout << "Original List: ";
+    printList(head);
+
+    Solution solution;
+    ListNode* reversedHead = solution.reverseList(head);
+
+    cout << "Reversed List: ";
+    printList(reversedHead);
+
+    return 0;
+}
